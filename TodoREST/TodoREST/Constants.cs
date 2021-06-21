0ead190ac639059c032ea13fe5401dddd6aedbfa
Your Name <you@example.com>
2021-06-20 19:33:54 -0500

    first commit

---
+++ TodoREST/TodoREST/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoREST
+{
+    public static class Constants
+    {
+
+        public static string RestUrl = "http://colegioquimico-001-site6.dtempurl.com/api/todoitems/{0}";
+    }
+}
---
+++ TodoREST/TodoREST/IHttpClientHandlerService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+
+namespace TodoREST
+{
+    public interface IHttpClientHandlerService
+    {
+        HttpClientHandler GetInsecureHandler();
+    }
+}
